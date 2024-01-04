1b78a43545eb13d9fedb2077531e92c9fc184b51
eddie1101 <erg1101@outlook.com>
2024-01-03 19:44:04 -0500

    Working on custom useanim

--- src/main/java/org/erg/deforestry/common/item/ChainsawItem.java
+++ src/main/java/org/erg/deforestry/common/item/ChainsawItem.java
@@ -1,5 +1,9 @@
 package org.erg.deforestry.common.item;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.BlockParticleOption;
 import net.minecraft.core.particles.DustParticleOptions;
@@ -12,6 +16,7 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
@@ -27,13 +32,16 @@ import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
+import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
 import org.erg.deforestry.Config;
 import org.erg.deforestry.common.registries.DeforestrySounds;
 import org.erg.deforestry.common.util.DeforestryUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class ChainsawItem extends Item {
 
@@ -42,8 +50,25 @@ public class ChainsawItem extends Item {
     }
 
     @Override
-    public @NotNull UseAnim getUseAnimation(ItemStack stack) {
-        return UseAnim.BOW;
+    public void initializeClient(Consumer<IClientItemExtensions> consumer) {
+        consumer.accept(new IClientItemExtensions() {
+
+
+
+//            @Nullable
+//            @Override
+//            public HumanoidModel.ArmPose getArmPose(LivingEntity entityLiving, InteractionHand hand, ItemStack itemStack) {
+//                return IClientItemExtensions.super.getArmPose(entityLiving, hand, itemStack);
+//            }
+
+            @Override
+            public boolean applyForgeHandTransform(PoseStack poseStack, LocalPlayer player, HumanoidArm arm, ItemStack itemInHand, float partialTick, float equipProcess, float swingProcess) {
+                poseStack.translate(0.1d, -0.15d, -0.25d);
+                poseStack.scale(0.75f, 0.75f, 0.75f);
+                poseStack.mulPose(Axis.XN.rotation((float)Math.PI / 4f));
+                return true;
+            }
+        });
     }
 
     @Override
